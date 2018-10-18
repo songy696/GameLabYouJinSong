9e78c5ca524db8af63cee5210fe63e0744505a76
You Jin Song <youjinsong@youjinsongsmbp.wireless.newschool.edu>
2018-10-18 18:38:48 -0400

    quiz

---
+++ Quiz - Flappy Bird/Assets/continuingBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class continuingBackground : MonoBehaviour {
+
+    public float speed = 1f;
+    private Vector3 newPosition;
+
+
+    // Use this for initialization
+    void Start () {
+
+        newPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosition.x += Time.deltaTime * speed;
+        transform.position = newPosition;
+    }
+
+
+}
---
+++ Quiz - Flappy Bird/Assets/repeatingBackgrounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class repeatingBackgrounds : MonoBehaviour
+{
+
+
+    public Vector2 speed = new Vector2(1,1);
+    public Vector2 direction = new Vector2(1, 0);
+    public bool isLinkedToCamera = false;
+
+
+    void Update(){
+
+
+        Vector3 movement = new Vector3(speed.x/5 * -direction.x, speed.y/5 * direction.y, 0);
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+
+        if (isLinkedToCamera)
+        {
+            Camera.main.transform.Translate(movement);
+        }
+    }
+
+}
